1f6c9af056dc52ce377abae753d3136bb2d0a9d2
Monwar Hossain <monwarhossainmunna@gmail.com>
2024-07-06 20:31:23 +0600

    [DONE]Create controller and route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { productRoutes } from "./app/modules/product/product.route";
+import { orderRoutes } from "./app/modules/order/order.route";
 
 // express
 const app: Application = express();
@@ -11,6 +12,7 @@ app.use(cors());
 
 // application routes
 app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/order", orderRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -1,1 +1,5 @@
-
+import express from "express";
+import { orderController } from "./order.controller";
+const router = express.Router();
+router.post("/create-order", orderController.createOrder);
+export const orderRoutes = router;
